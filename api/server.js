bf742c61b26e3796455ab6ecdac4e48a4adca72e
nithin <nithinpedamalla12@gmail.com>
2024-02-29 14:51:25 +0530

    initial commit

---
+++ api/server.js
@@ -0,0 +1,68 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoute from "./routes/user.route.js";
+import gigRoute from "./routes/gig.route.js";
+import orderRoute from "./routes/order.route.js";
+import conversationRoute from "./routes/conversation.route.js";
+import messageRoute from "./routes/message.route.js";
+import reviewRoute from "./routes/review.route.js";
+import authRoute from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+dotenv.config();
+mongoose.set("strictQuery", true);
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to mongoDB!");
+  } catch (error) {
+    console.log(error);
+  }
+};
+app.use(cors());
+
+// app.use(cors({ origin: "http://localhost:5173", allowedHeaders:"*",credentials: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   next();
+// });
+
+ app.use("/api/auth", authRoute);
+// app.use("/api/users", userRoute);
+// app.use("/api/gigs", gigRoute);
+// app.use("/api/orders", orderRoute);
+// app.use("/api/conversations", conversationRoute);
+// app.use("/api/messages", messageRoute);
+// app.use("/api/reviews", reviewRoute);
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+
+  return res.status(errorStatus).send(errorMessage);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!")
+})
+
+
+app.listen(8800, () => {
+  connect();
+  console.log("Backend server is running!");
+});
